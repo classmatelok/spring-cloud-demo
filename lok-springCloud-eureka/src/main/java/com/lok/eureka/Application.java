e116a7804e3bd806ce598e7e731f38489873d1fe
coxujiale <574262853@qq.com>
2017-10-17 17:36:50 +0800

    新增eureka-provider

--- lok-springCloud-eureka/src/main/java/com/lok/eureka/Application.java
+++ lok-springCloud-eureka/src/main/java/com/lok/eureka/Application.java
@@ -2,11 +2,15 @@ package com.lok.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author 启动类
  */
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableEurekaServer //开启注册中心服务
 public class Application {
 
 	public static void main(String[] args) {
