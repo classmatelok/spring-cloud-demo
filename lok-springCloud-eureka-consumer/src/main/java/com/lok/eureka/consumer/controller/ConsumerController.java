53c14cb34ff92232965624fd6c089c5fe73f6116
coxujiale <574262853@qq.com>
2017-11-09 17:13:14 +0800

    加入"@GetMapping"注解说明

--- lok-springCloud-eureka-consumer/src/main/java/com/lok/eureka/consumer/controller/ConsumerController.java
+++ lok-springCloud-eureka-consumer/src/main/java/com/lok/eureka/consumer/controller/ConsumerController.java
@@ -1,8 +1,10 @@
 package com.lok.eureka.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lok.eureka.consumer.api.ConsumerInterface;
@@ -18,7 +20,8 @@ public class ConsumerController {
 	@Autowired
 	private ConsumerInterface consumerInterface;
 	
-	@RequestMapping("/consumer/{name}")
+	@RequestMapping(value="/consumer/{name}", method=RequestMethod.GET)
+//	@GetMapping("/consumer/{name}") //相当于上行的简写，还有@PostMapping、@DeleteMapping、@PutMapping等，是spring4.3之后引入的
 	public String testProvider(@PathVariable("name") String name) {
 		return consumerInterface.testProvider(name);
 	}
